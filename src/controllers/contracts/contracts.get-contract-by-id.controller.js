3da8b870f4347869e1f3806ab233bd6a2edf79eb
Yaniv Kalfa <netameta1@yahoo.com>
2024-04-26 04:52:34 -0700

    fixed get contract by id

--- src/controllers/contracts/contracts.get-contract-by-id.controller.js
+++ src/controllers/contracts/contracts.get-contract-by-id.controller.js
@@ -1,11 +1,17 @@
 /**
- * FIX ME!
+ * @desc
+ * GET /contracts/:id - This API is broken 😵! it should return the contract only if it belongs to the profile calling. better fix that!
  * @returns contract by id
  */
 exports.getContractById = async function (req, res, next) {
   const {Contract} = req.app.get('models');
   const {id} = req.params;
-  const contract = await Contract.findOne({where: {id}});
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      ...{[req.profile.type === 'client' ? 'ClientId' : 'ContractorId']: req.profile.id}
+    }}
+  );
   if(!contract) return res.status(404).end();
   res.json(contract);
 };
