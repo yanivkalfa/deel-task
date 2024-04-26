4867571c1931f1d46c3226bc6bdaed277847f8fc
Yaniv Kalfa <netameta1@yahoo.com>
2024-04-26 05:01:57 -0700

    Added get all contracts

--- src/controllers/contracts/contracts.get-all-contracts.controller.js
+++ src/controllers/contracts/contracts.get-all-contracts.controller.js
@@ -1,4 +1,20 @@
+const { Op } = require('sequelize');
+/**
+ * @desc
+ * GET /contracts - Returns a list of contracts belonging to a user (client or contractor), the list should only contain non terminated contracts.
+ * @returns  all active contracts
+ */
 exports.getAllContracts = async function (req, res, next) {
-  //stuff
+  const {Contract} = req.app.get('models');
+  const contracts = await Contract.findAll({
+    where: {
+      status: {
+        [Op.not]: 'terminated',
+      },
+      ...{[req.profile.type === 'client' ? 'ClientId' : 'ContractorId']: req.profile.id}
+    }}
+  );
+  if(!contracts.length) return res.status(404).end();
+  res.json(contracts);
 };
 
